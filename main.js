dbdf14134cbfd0d06ac3cede0a105941ed098958
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-08-15 20:05:18 +0530

    design update and error handling added

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const inputArea=document.querySelector(".large-area--input");
-const outputArea=document.querySelector(".large-area--output");
-const btnFormat=document.querySelector(".controls__button--format");
-const btnMinify=document.querySelector(".controls__button--minify");
-btnFormat.addEventListener("click",()=>{
-    const formatted=JSON.stringify(JSON.parse(inputArea.value), null, 4);
-    outputArea.value=formatted;
+const inputArea = document.querySelector(".large-area--input");
+const outputArea = document.querySelector(".large-area--output");
+const btnFormat = document.querySelector(".controls__button--format");
+const btnMinify = document.querySelector(".controls__button--minify");
+
+btnFormat.addEventListener("click", () => {
+  try {
+    const formatted = JSON.stringify(JSON.parse(inputArea.value), null, 4);
+    outputArea.value = formatted;
+  } catch (error) {
+    console.error("Something went wrong", error)
+    window.alert("Something went Wrong , There should be some error in your JSON")
+  }
 })
-btnMinify.addEventListener("click",()=>{
-    const minified=JSON.stringify(JSON.parse(inputArea.value), null, 4);
-    outputArea.value=minified;
+
+btnMinify.addEventListener("click", () => {
+  try {
+    const minified = JSON.stringify(JSON.parse(inputArea.value), null, 4);
+    outputArea.value = minified;
+  } catch (error) {
+    console.error("Something went wrong", error)
+    window.alert("Something went Wrong , There should be some error in your JSON")
+  }
 });
